743f898341fc02290e630b3c47acb1cd7f48b6a8
aditya0sehgal <aditya0sehgal.dev@gmail.com>
2023-07-05 17:56:32 -0400

    added upload dataset feature and column type selection feature.

--- home.py
+++ home.py
@@ -1,13 +1,59 @@
 from flask import Flask, render_template, request
 import pandas as pd
-import random, numpy, csv
+import random, numpy, csv, io, os
 
 app = Flask(__name__)
 
+current_dataset_columns = []
+file_name = ""
 # Home landing page
-@app.route("/")
+@app.route("/" , methods=['GET'])
 def home():
-    return render_template('home.html')
+    datta = {
+        'dropdown' : ["item1", "item2", "item3", "item4", "item5" ],
+        'updatedata' : ""
+    }
+    return render_template('home.html', data = datta)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    global file_name
+    file = request.files['datasetFile']
+    if file:
+        destination_path = os.getcwd()  # Get the current working directory
+        file.save(os.path.join(destination_path, file.filename))
+        file_name = file.filename
+        print(file_name)
+        column_names = extract_column_names(file_name)
+        temparr = []
+        for col in column_names:
+            print(col)
+            temparr.append(col)
+        return {'column_names': temparr}
+    else:
+        return {'column_names': []}
+    
+
+def extract_column_names(file):
+    global current_dataset_columns
+    col_names = pd.read_csv(file).columns
+    current_dataset_columns = col_names
+    print(col_names)
+    return col_names
+
+# Post call based on no of questions input
+@app.route('/column-selection', methods=['POST'])
+def selectCatNumIgnore():
+    try:
+        global current_dataset_columns
+        # print(current_dataset_columns)
+        for col in current_dataset_columns:
+            print(col, request.form.get(col))
+        return render_template('generate.html')
+
+    except Exception as e:
+        return render_template('error.html')
+        
 
 # Post call based on no of questions input
 @app.route('/generate', methods=['POST'])
@@ -28,7 +74,7 @@ def generate():
             for attribute_no in range(random.choice(range(1, 5))):
                 random_column = random.choice(categorical_attr) 
                 base_question_str += " and "+ random_column +" = '"+random.choice(list(set(airbnb[random_column].values)))+"'"
-                
+
             if base_question_str not in duplicate_question_check:
                 question_list.append(base_question_str)
                 duplicate_question_check[base_question_str] = 1
@@ -39,11 +85,11 @@ def generate():
         questionsDF = pd.DataFrame(question_list, columns=['Questions'])
         questionsDF.to_csv("GeneratedQuestions.csv")
         questions_html = questionsDF.to_html()
-        return render_template('generated.html', data=questions_html)
+        return render_template('successful_csv_generation.html', data=questions_html)
     
     except Exception as e:
         return render_template('error.html')
-    
-    
+        
+
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
